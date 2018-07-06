e90e034eb1526a04f651940bd896c16723217ee4
sam1995 <samrudh.kumar@mountblue.io>
2018-07-06 17:44:56 +0530

    Add protos.js

---
+++ protos.js
@@ -0,0 +1,22 @@
+
+
+// o1 = {name: 'o1'}
+// console.log(o1) // look o1.__proto__
+
+// o2 = Object.create(o1);
+// console.log(o2); // look o2.__proto__
+
+// 'new' Pattern 
+
+// function CreateMyObject() {
+//     this.name = 'my Object'
+// }
+
+// CreateMyObject.prototype.prop = 'new prop';
+
+// // console.dir(CreateMyObject);
+
+
+// let newObject = new CreateMyObject();
+
+// console.log(newObject);
